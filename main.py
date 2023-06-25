dc799a0e5a2ce1ef66dbd012fff47cb5a82ec2f4
sakibtamim <sakib.tamim@gmail.com>
2023-06-25 15:46:26 +0600

    first commit

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import tkinter as tk
 def detect_lang():
     window = tk.Tk()
     window.geometry('600x400')
-    head = tk.Label(window, text='Language Detector', font='Calibri 15')
+    head = tk.Label(window, text='Language Detector', font='Calibri 18')
     head.pack(pady=20)
 
     def check_language():
         new_text = text.get()
         lang = detect(str(new_text))
-        tk.Label(window, text=lang, font='Calibri 15').place(x=260, y=200)
+        tk.Label(window, text=lang, font='Calibri 18').place(x=260, y=200)
 
     text = tk.StringVar()
     tk.Entry(window, textvariable=text).place(
